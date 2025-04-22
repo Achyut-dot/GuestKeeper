a0dcce0156c72ada39d868a8f3c782883e66b2ce
Achyut Raghuvanshi <achyut24122002@gmail.com>
2025-04-22 11:56:27 +0530

    Local Storage Implemented in Reservation Form

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -8,6 +8,11 @@ export class ReservationService {
 
   private reservations: Reservation[] = [];
 
+  constructor() {
+    let savedReservations = localStorage.getItem("reservations");
+    this.reservations = savedReservations ? JSON.parse(savedReservations) : [];
+  }
+
   //CRUD Operations
 
   getReservations(): Reservation[] {
@@ -20,15 +25,18 @@ export class ReservationService {
 
   addReservation(reservation: Reservation): void {
     this.reservations.push(reservation);
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   deleteReservation(id: string): void {
     let index = this.reservations.findIndex(res => res.id === id);
     this.reservations.splice(index, 1);
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 
   updateReservation(updateReservation: Reservation): void {
     let index = this.reservations.findIndex(res => res.id === updateReservation.id);
     this.reservations[index] = updateReservation;
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
   }
 }
